e9a23379bd986dfd92bf1a2b0b63ce11ece0df53
xapharius <mihai.suteu@xapharius.com>
2018-07-02 22:18:41 +0100

    switching from feature encoders to manual replace

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -4,8 +4,12 @@ import pandas as pd
 
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, LabelBinarizer, RobustScaler
 
+
 def _cabin_floor(df):
-    return df["cabin"].str[0].fillna("")
+    res = df["cabin"].str[0].fillna("X")
+    res = res.replace({"A": 9, "B": 7, "C": 6, "D": 5, 
+                       "E": 4, "F": 3, "G": 2, "T": 1, "X": 0})
+    return np.log(res + 1)
 
 
 def _names(df):
@@ -16,33 +20,37 @@ def _fare(df):
     return np.log10(df["fare"] + 1)
 
 
+def _sex(df):
+    return df["sex"].replace({"male": 1, "female": 0})
+
+
+def _embarked(df):
+    res = pd.DataFrame()
+    res["embarked_C"] = df["embarked"] == "C"
+    res["embarked_Q"] = df["embarked"] == "Q"
+    res["embarked_S"] = df["embarked"] == "S"
+    res["embarked_X"] = df["embarked"].isnull()
+    return res
+
+
 class Preprocessor(object):
     
     def __init__(self):
-        self.cabin_floor = LabelEncoder()
-        self.sex = LabelEncoder()
-        self.embarked = LabelBinarizer()
         self.name_feats = None
         self.scaler = RobustScaler()
     
     def _fit(self, df: pd.DataFrame):
-        self.cabin_floor = self.cabin_floor.fit(_cabin_floor(df))
-        self.sex = self.sex.fit(df["sex"])
-        
         names = _names(df)
         self.name_feats_gb = _fare(df).groupby(names).mean().to_frame() 
         self.name_feats_gb["count"] = np.log(df.groupby(names).size())
         self.name_feats_gb = self.name_feats_gb.reset_index()
-        
-        self.embarked = LabelBinarizer().fit(df["embarked"].fillna("X"))
-        
         return self
     
     def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
         res = pd.DataFrame(index=df.index)
-        res["cabin_floor"] = np.log(self.cabin_floor.transform(_cabin_floor(df)) + 1)
+        res["cabin_floor"] = _cabin_floor(df)
         res["pclass"] = df["pclass"]
-        res["sex"] = self.sex.transform(df["sex"])
+        res["sex"] = _sex(df)
         res["age"] = df["age"]
         res["fare"] = _fare(df)
         res[["sibsp", "parch"]] = np.log(df[["sibsp", "parch"]] + 1)
@@ -55,9 +63,7 @@ class Preprocessor(object):
         res["name_mean_fare"] = name_feats["fare"].fillna(0)
         res["name_count"] = name_feats["count"].fillna(0)
         
-        cols = ["embarked_" + str(i) for i in range(4)]
-        embarked = pd.DataFrame(self.embarked.transform(df["embarked"].fillna("X")), 
-                                columns=cols, index=df.index)
+        embarked = _embarked(df)
         res = pd.concat([res, embarked], axis=1)
         return res
     
